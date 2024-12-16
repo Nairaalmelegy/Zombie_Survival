626320e6343eb39d5cc24e7f453452edbdf6dae0
Nairaalmelegy <138025295+Nairaalmelegy@users.noreply.github.com>
2024-12-16 13:33:27 +0200

    adding variables needed

--- Character.h
+++ Character.h
@@ -6,6 +6,10 @@
 #include <GL/glut.h>
 #include <string>
 
+extern bool jumpKey;
+extern bool moveLeftKey;
+extern bool moveRightKey;
+
 class Character {
 private:
     static const int MAX_TEXTURES = 10; // Maximum number of textures
@@ -18,7 +22,14 @@ private:
     float animationSpeed;
     float lastUpdateTime;
     float Health;
-
+    bool isFacingLeft = false;
+    float groundLevel = -1.0;
+    float isOnGround = false;
+    //Screen boundaries
+    const float SCREEN_LEFT = -1.0f;
+    const float SCREEN_RIGHT = 1.0f;
+    const float SCREEN_BOTTOM = -1.0f;
+    const float SCREEN_TOP = 1.0f;
 public:
     // Constructor
     Character(float x, float y, float width, float height, float speedX, float speedY, float animationSpeed);
@@ -30,7 +41,9 @@ public:
     void loadTextures(const std::string texturePaths[], int count);
     void update();
     void draw() const;
-
+    void moveLeft();
+    void moveRight();
+    void jump(float jumpSpeed);
     float getX() const { return x; }
 
 };
