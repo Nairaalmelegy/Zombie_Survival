8f39ac80d2ad6f4aa6ed2e84573133f6130f9b9a
Nairaalmelegy <138025295+Nairaalmelegy@users.noreply.github.com>
2024-12-16 13:32:56 +0200

    applying changes done

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 #include "Zombie.h"
 
 Character* character;
-Zombie* zombie1;
+Zombie2* zombie1;
 Zombie2* zombie2;
+Zombie2* zombie3;
+bool moveLeftKey = false;
+bool moveRightKey = false;
+bool jumpKey = false;
 
 // Initialize OpenGL settings and load character
 void initialize() {
@@ -13,14 +17,12 @@ void initialize() {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // Enable transparency
 
     // Create a character
-    character = new Character(0.05f, -1.0f, 0.2f, 0.2f, 0.0f, 0.0f, 0.2f);
-
-    // Load textures for the character
+    character = new Character(0.05f, -1.0f, 0.2f, 0.2f, 0.0005f, 0.0005f, 0.2f);
     const std::string texturePaths[] = { "images/Hero/Pose_1.png", "images/Hero/Pose_2.png" };
-    character->loadTextures(texturePaths, 2); // Provide array and count
+    character->loadTextures(texturePaths, 2); 
 
     // Create Zombie
-    zombie1 = new Zombie(0.5f, -1.0f, 0.1f, 0.2f, 0.001f, 0.005f, 1.0f, character);
+    zombie1 = new Zombie2(0.5f, -1.0f, 0.1f, 0.2f, 0.0005f, 0.0005f, 1.0f, character);
     const std::string zombieTextures[] = {
         "images/Low_Zombie/Z_1/Level_1-1.png",
         "images/Low_Zombie/Z_1/Level_1-2.png",
@@ -29,8 +31,8 @@ void initialize() {
     };
     zombie1->loadZombie(zombieTextures, 4);
 
-
-    zombie2 = new Zombie2(-1.0f, -1.0f, 0.1f, 0.2f, 0.001f, 0.005f, 1.0f, character);
+    // Create Zombie2
+    zombie2 = new Zombie2(-1.0f, -1.0f, 0.1f, 0.2f, 0.0005f, 0.0005f, 1.0f, character);
     const std::string zombie2Textures[] = {
         "images/Low_Zombie/Z_2/Zombie_2_1.png",
         "images/Low_Zombie/Z_2/Zombie_2_2.png",
@@ -38,12 +40,21 @@ void initialize() {
     };
     zombie2->loadZombie(zombie2Textures, 3);
 
+
+    // Create Zombie3
+    zombie3 = new Zombie2(0.5f, -0.5f, 0.1f, 0.2f, 0.0005f, 0.0005f, 1.0f, character);
+    const std::string zombie3Textures[] = {
+        "images/Low_Zombie/Z_3/Zombie_3_1.png",
+        "images/Low_Zombie/Z_3/Zombie_3_2.png",
+        
+    };
+    zombie3->loadZombie(zombie3Textures, 2);
 }
 
 // Display function
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-
+    
     character->update();
     character->draw();
 
@@ -53,6 +64,9 @@ void display() {
 
     zombie2->update();
     zombie2->draw(); // Draw the second zombie
+
+    zombie3->update();
+    zombie3->draw();
     glutSwapBuffers();
 }
 
@@ -61,6 +75,7 @@ void cleanup() {
     delete character;
     delete zombie1;
     delete zombie2;
+    delete zombie3;
 }
 
 void exitProgram() {
@@ -73,6 +88,67 @@ void keyboard(unsigned char key, int x, int y) {
         exitProgram();
     }
 }
+/*--------------------------------------------------Control movement by keyboard--------------------------------*/
+void handleKeyPress(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'a': // Move left
+    case 'A':
+        moveLeftKey = true;
+        break;
+    case 'd': // Move right
+    case 'D':
+        moveRightKey = true;
+        break;
+    case ' ': // Jump
+        jumpKey = true;
+        break;
+    }
+}
+
+void handleKeyRelease(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'a': // Stop moving left
+    case 'A':
+        moveLeftKey = false;
+        break;
+    case 'd': // Stop moving right
+    case 'D':
+        moveRightKey = false;
+        break;
+    case ' ': // Stop jumping
+        jumpKey = false;
+        break;
+    }
+}
+
+void handleSpecialKeyPress(int key, int x, int y) {
+    switch (key) {
+    case GLUT_KEY_LEFT: // Move left
+        moveLeftKey = true;
+        break;
+    case GLUT_KEY_RIGHT: // Move right
+        moveRightKey = true;
+        break;
+    case GLUT_KEY_UP: // Jump
+        jumpKey = true;
+        break;
+    }
+}
+
+void handleSpecialKeyRelease(int key, int x, int y) {
+    switch (key) {
+    case GLUT_KEY_LEFT: // Stop moving left
+        moveLeftKey = false;
+        break;
+    case GLUT_KEY_RIGHT: // Stop moving right
+        moveRightKey = false;
+        break;
+    case GLUT_KEY_UP: // Jump stop
+        jumpKey = false;
+        break;
+    }
+}
+
 
 // Main function
 int main(int argc, char** argv) {
@@ -80,6 +156,16 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
     glutCreateWindow("Character Animation");
+
+    // Register display and update callbacks
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+
+    // Register keyboard callbacks
+    glutKeyboardFunc(handleKeyPress);
+    glutKeyboardUpFunc(handleKeyRelease);
+    glutSpecialFunc(handleSpecialKeyPress);
+    glutSpecialUpFunc(handleSpecialKeyRelease);
 
     initialize();
     atexit(cleanup); // Register cleanup function
